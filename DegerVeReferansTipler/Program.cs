15e3b98b9688a42e4495ac29c53a59d70bf57370
Alper Can <a.can.erysr@gmail.com>
2022-07-05 17:14:13 +0300

    sadasdasda

--- DegerVeReferansTipler/Program.cs
+++ DegerVeReferansTipler/Program.cs
@@ -11,7 +11,7 @@ namespace DegerVeReferansTipler
             sayı1 = sayı2;
             sayı2 = 65;
             Console.WriteLine(sayı1);
-
+            Console.WriteLine(sayı2);
             int[] sayilar1 = new int[] { 10, 20, 30 };
             int[] sayilar2 = new int[] { 100,200,300 };
             sayilar1 = sayilar2;
