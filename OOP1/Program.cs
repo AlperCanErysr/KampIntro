7542b90a3e86651f908a856aff0b1c713f85aacf
Alper Can <a.can.erysr@gmail.com>
2022-07-06 09:36:15 +0300

    sdasdasdasd

--- OOP1/Program.cs
+++ OOP1/Program.cs
@@ -6,7 +6,20 @@ namespace OOP1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Product product1 = new Product();
+            product1.Id = 123;
+            product1.CategoryId = 2;
+            product1.ProductName = "Computer";
+            product1.UnitPrice = 20000;
+            product1.UnitsInStock = 50;{};
+
+            Product product2 = new Product { Id = 2, CategoryId   =5,UnitsInStock=5,ProductName="Phone",UnitPrice=10000 };
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+            Console.WriteLine(product1.ProductName);
+
+
         }
     }
 }
